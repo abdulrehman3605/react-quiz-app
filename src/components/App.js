9d2b44ed1970d830d39b5a6f985ecf81059d311e
Abdul Rehman <125661461+abdulrehman3605@users.noreply.github.com>
2024-01-31 21:22:26 +0500

    changes

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  quesion: [],
+
+  status: "loading",
+};
+
+function reducer(action, state) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("case not exists");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Next</p>
+      </Main>
+    </div>
+  );
+}
